a618551742a89a3f78e914b69a85088d1fb560c8
prakashMTD <prakashmt95@gmail.com>
2022-07-16 20:30:10 +0530

    Fist Push master

---
+++ src/org/tec/Sample.java
@@ -0,0 +1,26 @@
+package org.tec;
+
+public class Sample {
+	private void method1() {
+		System.out.println("Type 11");
+
+	}
+
+	private void method2() {
+		System.out.println("Type 22");
+
+	}
+
+	private void method3() {
+		System.out.println("Type 33");
+
+	}
+	private void method4() {
+		System.out.println("Type 44");
+
+	}
+	private void method5() {
+		System.out.println("Type 55");
+
+	}
+}
